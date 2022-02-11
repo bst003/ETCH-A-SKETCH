c0e921111e619de0d1567225d02f4d8941394b3a
bst003 <bstanner33@yahoo.com>
2022-02-10 21:42:26 -0500

    Set up basic structure, write out plan

---
+++ scripts.js
@@ -0,0 +1,20 @@
+/*
+
+PLANNING
+
+Increment opacity of background by .1 for each pass over?
+
+The sidebar will contain all of the controls
+    - Button for black mode (selected by default)
+    - Button for rainbow mode
+    - Button for eraser mode
+    - Range input for determing board size (16 block min, 100 block max)
+        - Add note to notify user that board will clear if board size is changed
+    - Button to clear the board
+
+#eas-board will be the container for the etch-a-sketch
+    - Will get value of range input to determine size
+    - Will use display grid to align the divs inside
+    
+
+*/
